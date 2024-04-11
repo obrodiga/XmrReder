c42f2c7cf77b236980517bac9ee454e6d9f0eff3
obrodiga <gelodelo1g@gmail.com>
2024-04-11 17:16:15 +0500

    Комментарии для кода

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,37 +16,38 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    QXmlStreamReader XmlReader;
-    XmlReader.addData(ui->textEdit->toPlainText());
-    ui->treeWidget->clear();
-    QStack<QTreeWidgetItem*> StackTags;
+    QXmlStreamReader XmlReader;//элемент класса для работы с XML файлами
+    XmlReader.addData(ui->textEdit->toPlainText());//получение введённых данных
+    QStack<QTreeWidgetItem*> StackTags; //список элементов для создания иерархии
 
-    while (!XmlReader.atEnd())
+    ui->treeWidget->clear();//очистка поля для вывода тегов
+
+    while (!XmlReader.atEnd())//перебор всех элементов
     {
-        if (XmlReader.isStartElement())
+        if (XmlReader.isStartElement())//проверка на открывающий элемент
         {
             QStringList Tags;
-            Tags<<XmlReader.name().toString();
-            QTreeWidgetItem* item = new QTreeWidgetItem(Tags);
+            Tags<<XmlReader.name().toString();//получение имени текущего элемента
+            QTreeWidgetItem* item = new QTreeWidgetItem(Tags); //создание элементов для последующего добавления в дерево
             if (StackTags.count()==0)
             {
-                ui->treeWidget->addTopLevelItem(item);
+                ui->treeWidget->addTopLevelItem(item);//добавление корневых элементов в дерево
             }
             else
             {
-                StackTags.top()->addChild(item);
+                StackTags.top()->addChild(item);//добавление дочернего элемента
             }
             StackTags.push_back(item);
         }
-        if (XmlReader.isEndElement())
+        if (XmlReader.isEndElement())//закрывающие элементы
         {
-            StackTags.pop();
+            StackTags.pop();//уадление верхнего элемнета из стека
         }
-        XmlReader.readNext();
+        XmlReader.readNext();//переход к следующему элементу
     }
     if (XmlReader.hasError())
     {
-        ui->textBrowser->setPlainText(XmlReader.errorString());
+        ui->textBrowser->setPlainText(XmlReader.errorString());//вывод ошибки
     }
 }
 
