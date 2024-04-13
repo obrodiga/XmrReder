4a5c8d5dc256e0f95f9e0ab187f18f9ea7444d24
obrodiga <gelodelo1g@gmail.com>
2024-04-13 17:03:37 +0500

    Открытие Файлов

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -21,6 +21,7 @@ void MainWindow::on_pushButton_clicked()
     QStack<QTreeWidgetItem*> StackTags; //список элементов для создания иерархии
 
     ui->treeWidget->clear();//очистка поля для вывода тегов
+    ui->textBrowser->clear();
 
     while (!XmlReader.atEnd())//перебор всех элементов
     {
@@ -48,6 +49,50 @@ void MainWindow::on_pushButton_clicked()
     if (XmlReader.hasError())
     {
         ui->textBrowser->setPlainText(XmlReader.errorString());//вывод ошибки
+    }
+}
+
+void MainWindow::on_FileOpen_clicked()
+{
+    QString FileName;
+    QStack<QTreeWidgetItem*> StackTags;
+    QXmlStreamReader XmlReader;
+    FileName = QFileDialog::getOpenFileName(this, tr("Open XML File"), "", tr("XML file (*.xml)"));
+    if (FileName != "")
+    {
+        ui->FileFolder->setText(FileName);
+        ui->treeWidget->clear();
+        ui->textBrowser->clear();//очистка поля ошибок
+        //QFile file(ui->FileFolder->text());
+        //XmlReader.setDevice(&file);
+        XmlReader.readNext();
+        while (!XmlReader.atEnd())
+        {
+            if (XmlReader.isStartElement())
+            {
+                QStringList Tags;
+                Tags<<XmlReader.name().toString();
+                QTreeWidgetItem* item = new QTreeWidgetItem(Tags);
+                if (StackTags.count()==0)
+                {
+                    ui->treeWidget->addTopLevelItem(item);
+                }
+                else
+                {
+                    StackTags.top()->addChild(item);//добавление дочернего элемента
+                }
+                StackTags.push_back(item);
+            }
+            if (XmlReader.isEndElement())//закрывающие элементы
+            {
+                StackTags.pop();//уадление верхнего элемнета из стека
+            }
+            XmlReader.readNext();//переход к следующему элементу
+        }
+        if (XmlReader.hasError())
+        {
+            ui->textBrowser->setPlainText(XmlReader.errorString());//вывод ошибки
+        }
     }
 }
 
