d9a03c40ed94dc8270f30a8f12d3ec036e4db374
obrodiga <gelodelo1g@gmail.com>
2024-04-14 11:39:52 +0500

    UI appdate

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -54,50 +54,59 @@ void MainWindow::on_pushButton_clicked()
 
 void MainWindow::on_FileOpen_clicked()
 {
-    QString FileName;
-    FileName = QFileDialog::getOpenFileName(this, tr("Open XML File"), "", tr("XML file (*.xml)"));
-    ui->FileFolder->setText(FileName);
-    QFile file(ui->FileFolder->text());
-
-    if (!file.open(QFile::ReadOnly | QFile::Text))
+    bool Check=ui->checkBox->checkState();
+    if (Check)
     {
-        ui->textBrowser->clear();
-        ui->textBrowser->setPlainText("Не удалось открыть файл");
+        QString FileName;
+        FileName = QFileDialog::getOpenFileName(this, tr("Open XML File"), "", tr("XML file (*.xml)"));
+        ui->FileFolder->setText(FileName);
+        QFile file(ui->FileFolder->text());
+
+        if (!file.open(QFile::ReadOnly | QFile::Text))
+        {
+            ui->textBrowser->clear();
+            ui->textBrowser->setPlainText("Не удалось открыть файл");
+        }
+        else
+        {
+            ui->treeWidget->clear();
+            ui->textBrowser->clear();//очистка поля ошибок
+            QStack<QTreeWidgetItem*> StackTags;
+            QXmlStreamReader XmlReader;
+            XmlReader.setDevice(&file);//выставление файла откуда происходит чтение
+            XmlReader.readNext();
+            while (!XmlReader.atEnd())
+            {
+                if (XmlReader.isStartElement())
+                {
+                    QStringList Tags;
+                    Tags<<XmlReader.name().toString();
+                    QTreeWidgetItem* item = new QTreeWidgetItem(Tags);
+                    if (StackTags.count()==0)
+                    {
+                        ui->treeWidget->addTopLevelItem(item);
+                    }
+                    else
+                    {
+                        StackTags.top()->addChild(item);//добавление дочернего элемента
+                    }
+                    StackTags.push_back(item);
+                }
+                if (XmlReader.isEndElement())//закрывающие элементы
+                {
+                    StackTags.pop();//уадление верхнего элемнета из стека
+                }
+                XmlReader.readNext();//переход к следующему элементу
+            }
+            if (XmlReader.hasError())
+            {
+                ui->textBrowser->setPlainText(XmlReader.errorString());//вывод ошибки
+            }
+        }
     }
     else
     {
-        ui->treeWidget->clear();
-        ui->textBrowser->clear();//очистка поля ошибок
-        QStack<QTreeWidgetItem*> StackTags;
-        QXmlStreamReader XmlReader;
-        XmlReader.setDevice(&file);//выставление файла откуда происходит чтение
-        XmlReader.readNext();
-        while (!XmlReader.atEnd())
-        {
-            if (XmlReader.isStartElement())
-            {
-                QStringList Tags;
-                Tags<<XmlReader.name().toString();
-                QTreeWidgetItem* item = new QTreeWidgetItem(Tags);
-                if (StackTags.count()==0)
-                {
-                    ui->treeWidget->addTopLevelItem(item);
-                }
-                else
-                {
-                    StackTags.top()->addChild(item);//добавление дочернего элемента
-                }
-                StackTags.push_back(item);
-            }
-            if (XmlReader.isEndElement())//закрывающие элементы
-            {
-                StackTags.pop();//уадление верхнего элемнета из стека
-            }
-            XmlReader.readNext();//переход к следующему элементу
-        }
-        if (XmlReader.hasError())
-        {
-            ui->textBrowser->setPlainText(XmlReader.errorString());//вывод ошибки
-        }
+        ui->FileFolder->clear();
+        ui->FileFolder->setText("Для использования локальных файлов отметьте данную настройку");
     }
 }
