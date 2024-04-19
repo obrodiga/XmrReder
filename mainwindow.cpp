dfa83caecdd308d3c0873250a3b947b56b2fb3cc
obrodiga <gelodelo1g@gmail.com>
2024-04-19 20:19:53 +0500

    Squashed commit of the following:
    commit a7c122c50d4b7fd0449767aa4d395cdc3218aaef
    Author: obrodiga <gelodelo1g@gmail.com>
    Date:   Fri Apr 19 20:06:34 2024 +0500
    
        Расширение взаимодействия с тегами
    
    commit 5c10bf12206a336a59bb139d751618388cda3dbf
    Author: obrodiga <gelodelo1g@gmail.com>
    Date:   Thu Apr 18 22:17:34 2024 +0500
    
        Добавление атрибутов в дерево тегов
    
    commit 1fd75ade7113b84c42096eecb1a18bd1e5e45447
    Author: obrodiga <gelodelo1g@gmail.com>
    Date:   Thu Apr 18 17:58:32 2024 +0500
    
        Сохранение атрибутов для всех тегов
    
    commit 7f82efe34ab8a10997bf26bbbe0ad0f8894e3c9c
    Author: obrodiga <gelodelo1g@gmail.com>
    Date:   Thu Apr 18 16:47:44 2024 +0500
    
        Первичное чтение атрибутов

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -57,10 +57,12 @@ void MainWindow::on_FileOpen_clicked()
     bool Check=ui->checkBox->checkState();
     if (Check)
     {
+        int itemCount=0;
         QString FileName;
         FileName = QFileDialog::getOpenFileName(this, tr("Open XML File"), "", tr("XML file (*.xml)"));
         ui->FileFolder->setText(FileName);
         QFile file(ui->FileFolder->text());
+        ui->textEdit->clear();
 
         if (!file.open(QFile::ReadOnly | QFile::Text))
         {
@@ -73,34 +75,137 @@ void MainWindow::on_FileOpen_clicked()
             ui->textBrowser->clear();//очистка поля ошибок
             QStack<QTreeWidgetItem*> StackTags;
             QXmlStreamReader XmlReader;
+            QString TempStr;
+            //QVector<QString> atributs;
+            QXmlStreamAttributes attrib;
             XmlReader.setDevice(&file);//выставление файла откуда происходит чтение
             XmlReader.readNext();
+
             while (!XmlReader.atEnd())
             {
+                bool flagAtr=false;
+                attrib=XmlReader.attributes();
+                TempStr.clear();
+                if (XmlReader.name().toString()=="logical_device")
+                {
+                    if(attrib.hasAttribute("address"))
+                    {
+                        TempStr="address = ";
+                        TempStr += attrib.value("address").toString();
+                    }
+                    if (attrib.hasAttribute("module"))
+                    {
+                        TempStr+="  module = ";
+                        TempStr += attrib.value("module").toString();
+                    }
+                    if (TempStr!=NULL)
+                    {
+                        TempStr="logical_device  "+TempStr;
+                    }
+                }
+                if (XmlReader.name().toString()=="line")
+                {
+                    if(attrib.hasAttribute("type"))
+                    {
+                        TempStr="type = ";
+                        TempStr += attrib.value("type").toString();
+                    }
+                    if (attrib.hasAttribute("port"))
+                    {
+                        TempStr+="  port = ";
+                        TempStr += attrib.value("port").toString();
+                    }
+                    if (attrib.hasAttribute("address"))
+                    {
+                        TempStr+="  address = ";
+                        TempStr += attrib.value("address").toString();
+                    }
+                    if (TempStr!=NULL)
+                    {
+                        TempStr="line  "+TempStr;
+                    }
+                }
+                if (XmlReader.name().toString()=="object")
+                {
+                    if(attrib.hasAttribute("class_id"))
+                    {
+                        TempStr="class_id = ";
+                        TempStr += attrib.value("class_id").toString();
+                    }
+                    if (attrib.hasAttribute("logical_name"))
+                    {
+                        TempStr+="  logical_name = ";
+                        TempStr += attrib.value("logical_name").toString();
+                    }
+                    if (attrib.hasAttribute("data_source"))
+                    {
+                        TempStr+="  data_source = ";
+                        TempStr += attrib.value("data_source").toString();
+                    }
+                    if (TempStr!=NULL)
+                    {
+                        TempStr="object  "+TempStr;
+                    }
+                }
+                if (TempStr!=NULL)
+                {
+                    flagAtr=true;
+                    ui->textBrowser->append(TempStr);
+                }
+                else
+                {
+                    TempStr=XmlReader.name().toString();
+                }
+
                 if (XmlReader.isStartElement())
                 {
                     QStringList Tags;
-                    Tags<<XmlReader.name().toString();
+                    Tags<<TempStr;
                     QTreeWidgetItem* item = new QTreeWidgetItem(Tags);
+
                     if (StackTags.count()==0)
                     {
                         ui->treeWidget->addTopLevelItem(item);
                     }
                     else
                     {
-                        StackTags.top()->addChild(item);//добавление дочернего элемента
+                        if (flagAtr)
+                        {
+                            StackTags.top()->addChild(item);
+                        }
+                        else
+                        {
+                            StackTags.top()->addChild(item);//добавление дочернего элемента
+                        }
+
                     }
                     StackTags.push_back(item);
+                    TempStr="<"+TempStr;
+                    for (int i=0; i<itemCount;i++)
+                    {
+                        TempStr="   "+TempStr;
+                    }
+                    TempStr+=">";
+                    ui->textEdit->append(TempStr);
+                    itemCount+=1;
                 }
                 if (XmlReader.isEndElement())//закрывающие элементы
                 {
                     StackTags.pop();//уадление верхнего элемнета из стека
+                    TempStr="<"+TempStr;
+                    for (int i=0; i<itemCount;i++)
+                    {
+                        TempStr="   "+TempStr;
+                    }
+                    TempStr+=">";
+                    ui->textEdit->append(TempStr);
+                    itemCount-=1;
                 }
                 XmlReader.readNext();//переход к следующему элементу
             }
             if (XmlReader.hasError())
             {
-                ui->textBrowser->setPlainText(XmlReader.errorString());//вывод ошибки
+                ui->textBrowser->append(XmlReader.errorString());//вывод ошибки
             }
         }
     }
